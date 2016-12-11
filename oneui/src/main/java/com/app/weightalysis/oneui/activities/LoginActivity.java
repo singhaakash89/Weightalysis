4e6a74e98b6c03f927fa50f8cb94d0c4dfe27e23
singhaakash89 <singhaakash89@gmail.com>
2016-12-12 00:36:28 +0530

    latest

---
+++ oneui/src/main/java/com/app/weightalysis/oneui/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.app.weightalysis.oneui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aakash Singh on 12-12-2016.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
